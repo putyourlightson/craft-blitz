da79e0fd745e88282a78b5da2e59811b786ea98f
Ben Croker <ben@putyourlightson.net>
2018-11-09 09:25:52 +0100

    Added sample config.php file

---
+++ src/config.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @copyright Copyright (c) PutYourLightsOn
+ */
+
+/**
+ * Blitz config.php
+ *
+ * This file exists only as a template for the Blitz settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'blitz.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    // Whether static file caching should be enabled.
+    //'cachingEnabled' => false,
+
+    // Whether the cache should automatically be warmed after clearing.
+    //'warmCacheAutomatically' => 'true',
+
+    // Whether URLs with query strings should cached. Disabling this will not cache any URLs that contain query strings.
+    //'queryStringCachingEnabled' => false,
+
+    // The relative path to your cache folder from your public webroot.
+    //'cacheFolderPath' => 'cache/blitz',
+
+    // The URI patterns to include in static file caching.
+    //'includedUriPatterns' => [["pages/.*"], ["articles/.*"]],
+
+    // The URI patterns to exclude from static file caching. This will override any matching patterns to include.
+    //'excludedUriPatterns' => [["contact"]],
+];
