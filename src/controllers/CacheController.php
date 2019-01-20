ae2d9bdc57c3d43e0c198b1dd13b59fee2b8ae08
Ben Croker <ben@putyourlightson.net>
2019-01-20 16:03:10 +0100

    2.0.0-beta.3.1

--- src/controllers/CacheController.php
+++ src/controllers/CacheController.php
@@ -24,6 +24,16 @@ class CacheController extends Controller
     /**
      * @inheritdoc
      */
+    public $enableCsrfValidation = false;
+
+    /**
+     * @var bool Disable Snaptcha validation
+     */
+    public $enableSnaptchaValidation = false;
+
+    /**
+     * @inheritdoc
+     */
     protected $allowAnonymous = true;
 
     // Public Methods
