e37bb4638e805407bbd4949280c6c9763e8966f1
bencroker <ben@putyourlightson.net>
2020-07-16 10:32:52 +0200

    Use IE compatible events

--- src/resources/js/blitzInjectScript.js
+++ src/resources/js/blitzInjectScript.js
@@ -1,7 +1,7 @@
 var Blitz = {};
 
 // The event name will be replaced with the `injectScriptEvent` config setting.
-document.addEventListener("{injectScriptEvent}", blitzInject);
+document.addEventListener('{injectScriptEvent}', blitzInject);
 
 function blitzInject() {
     "use strict";
@@ -13,9 +13,9 @@ function blitzInject() {
         }
     };
 
-    var event = new CustomEvent("beforeBlitzInjectAll", {
-        cancelable: true
-    });
+    // Use IE compatible events (https://caniuse.com/#feat=customevent)
+    var event = document.createEvent('CustomEvent');
+    event.initCustomEvent('beforeBlitzInjectAll', false, true, null);
 
     if (!document.dispatchEvent(event)) {
         return;
@@ -26,39 +26,43 @@ function blitzInject() {
         var uri = data.getAttribute('data-blitz-uri');
         var params = data.getAttribute('data-blitz-params');
 
-        var customEventInit = {
-            detail: {
-                id: id,
-                uri: uri,
-                params: params
-            },
-            cancelable: true
+        var detail = {
+            id: id,
+            uri: uri,
+            params: params
         };
 
-        if (!document.dispatchEvent(new CustomEvent("beforeBlitzInject", customEventInit))) {
+        var event = document.createEvent('CustomEvent');
+        event.initCustomEvent('beforeBlitzInject', false, true, detail);
+
+        if (!document.dispatchEvent(event)) {
             return;
         }
 
         var xhr = new XMLHttpRequest();
         xhr.onload = function() {
             if (xhr.status >= 200 && xhr.status < 300) {
-                var element = document.getElementById("blitz-inject-" + id);
+                var element = document.getElementById('blitz-inject-' + id);
 
                 if (element) {
-                    customEventInit.detail.element = element;
-                    customEventInit.detail.responseText = this.responseText;
+                    detail.element = element;
+                    detail.responseText = this.responseText;
 
                     element.innerHTML = this.responseText;
                     element.classList.add('blitz-inject--injected');
                 }
 
-                document.dispatchEvent(new CustomEvent("afterBlitzInject", customEventInit));
+                var event = document.createEvent('CustomEvent');
+                event.initCustomEvent('afterBlitzInject', false, false, detail);
+                document.dispatchEvent(event);
             }
 
             Blitz.inject.loaded++;
 
             if (Blitz.inject.loaded >= Blitz.inject.data.length) {
-                document.dispatchEvent(new CustomEvent("afterBlitzInjectAll"));
+                var event = document.createEvent('CustomEvent');
+                event.initCustomEvent('afterBlitzInjectAll', false, false, null);
+                document.dispatchEvent(event);
             }
         };
 
