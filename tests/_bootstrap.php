b3d11f8d7fdaa55e9cd9e3795e3cfd0fb9ce795e
bencroker <ben@putyourlightson.net>
2020-07-27 10:03:44 +0200

    Fixed tests path

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,6 +4,7 @@ use craft\test\TestSetup;
 
 ini_set('date.timezone', 'UTC');
 
+define('CRAFT_TESTS_PATH', __DIR__);
 define('CRAFT_STORAGE_PATH', __DIR__ . '/_craft/storage');
 define('CRAFT_TEMPLATES_PATH', __DIR__ . '/_craft/templates');
 define('CRAFT_CONFIG_PATH', __DIR__ . '/_craft/config');
