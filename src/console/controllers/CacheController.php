dfa251a0a94f5165985696236ec8a39ca209400e
bencroker <ben@putyourlightson.net>
2022-04-04 12:50:26 +0100

    Improved output

--- src/console/controllers/CacheController.php
+++ src/console/controllers/CacheController.php
@@ -392,7 +392,7 @@ class CacheController extends Controller
         $total = count($siteUris);
 
         if ($generated < $total) {
-            $this->stdout(Craft::t('blitz', 'Generated {generated} of {total} pages. To see why pages were not cached, enable the `debug` config setting and then open the `storage/logs/blitz.log` file.', ['generated' => $generated, 'total' => $total]) . PHP_EOL, BaseConsole::FG_CYAN);
+            $this->stdout(Craft::t('blitz', 'Generated {generated} of {total} total possible pages. To see why some pages were not cached, enable the `debug` config setting and then open the `storage/logs/blitz.log` file.', ['generated' => $generated, 'total' => $total]) . PHP_EOL, BaseConsole::FG_CYAN);
         }
 
         $this->_output('Blitz cache generation complete.');
