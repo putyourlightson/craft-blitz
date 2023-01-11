1bdc9f5ed4d5bf7c8e87f5a3fb2b9d5f65238a60
bencroker <ben@putyourlightson.com>
2023-01-11 09:06:09 -0600

    Remove function and use constants in rewrite.php

--- src/rewrite.php
+++ src/rewrite.php
@@ -19,62 +19,71 @@ use putyourlightson\blitz\variables\BlitzVariable;
  * ```php
  * // Load Blitz rewrite
  * require CRAFT_VENDOR_PATH . '/putyourlightson/craft-blitz/src/rewrite.php';
- * blitzRewrite();
  * ```
  *
- * If the `Query String Caching` setting is set to `Cache URLs with query strings
- * as the same page` then pass in `false` as the first parameter.
+ * You can configure the rewrite by defining one or more of the available constants.
+ * For example, if the `Query String Caching` setting is set to `Cache URLs with query
+ * strings as the same page`, then set `BLITZ_UNIQUE_QUERY_STRINGS` to `false`.
  *
  * ```php
  * // Load Blitz rewrite
+ * define('BLITZ_UNIQUE_QUERY_STRINGS', true);
+ * define('BLITZ_CACHE_FOLDER_PATH', 'cache/blitz');
  * require CRAFT_VENDOR_PATH . '/putyourlightson/craft-blitz/src/rewrite.php';
- * blitzRewrite(false);
  * ```
  *
  * @since 4.3.0
- * @param bool $withQueryString Whether the query string should be included.
- * @param string|null $cacheFolderPath The cache folder path, if different to the default.
  */
-function blitzRewrite(bool $withQueryString = true, string $cacheFolderPath = null): void
-{
-    if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-        return;
-    }
 
-    $config = require CRAFT_BASE_PATH . '/config/general.php';
-    $tokenParam = $config instanceof GeneralConfig ? $config->tokenParam : ($config['tokenParam'] ?? 'token');
-    if (!empty($_GET[$tokenParam])) {
-        return;
-    }
-
-    $host = str_replace(':', '', $_SERVER['HTTP_HOST']);
-
-    $uri = $_SERVER['REQUEST_URI'];
-    if ($withQueryString) {
-        $uri = str_replace('?', '/', $uri);
-    } else {
-        $uri = strtok($uri, '?');
-    }
-
-    /**
-     * Modify the URI for include action requests.
-     * @see CacheRequestService::getRequestedCacheableSiteUri()
-     */
-    $action = $_GET['action'] ?? null;
-    if ($action === BlitzVariable::INCLUDE_ACTION) {
-        $uri = CacheRequestService::INCLUDES_FOLDER . '?' . http_build_query($_GET);
-    } elseif ($action === BlitzVariable::DYNAMIC_INCLUDE_ACTION) {
-        $uri = http_build_query($_GET);
-    }
-
-    $cacheFolderPath = $cacheFolderPath ?? CRAFT_BASE_PATH . '/web/cache/blitz';
-    $path = $cacheFolderPath . '/' . $host . $uri . '/index.html';
-    $path = str_replace(['//', '..'], ['/', ''], $path);
-
-    if (!is_file($path)) {
-        return;
-    }
-
-    echo file_get_contents($path);
-    exit();
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    return;
 }
+
+$config = require CRAFT_BASE_PATH . '/config/general.php';
+if ($config instanceof GeneralConfig) {
+    $tokenParam = $config->tokenParam;
+}
+else {
+    $tokenParam = $config['tokenParam'] ?? $config[CRAFT_ENVIRONMENT]['tokenParam'] ?? $config['*']['tokenParam'] ?? 'token';
+}
+if (!empty($_GET[$tokenParam])) {
+    return;
+}
+
+$tokenParam = defined('BLITZ_TOKEN_PARAM') ? BLITZ_TOKEN_PARAM : 'token';
+if (!empty($_GET[$tokenParam])) {
+    return;
+}
+
+$host = str_replace(':', '', $_SERVER['HTTP_HOST']);
+$uri = $_SERVER['REQUEST_URI'];
+
+/** @noinspection PhpTernaryExpressionCanBeReplacedWithConditionInspection */
+$withQueryString = defined('BLITZ_UNIQUE_QUERY_STRINGS') ? BLITZ_UNIQUE_QUERY_STRINGS : true;
+if ($withQueryString) {
+    $uri = str_replace('?', '/', $uri);
+} else {
+    $uri = strtok($uri, '?');
+}
+
+/**
+ * Modify the URI for include action requests.
+ * @see CacheRequestService::getRequestedCacheableSiteUri()
+ */
+$action = $_GET['action'] ?? null;
+if ($action === BlitzVariable::INCLUDE_ACTION) {
+    $uri = CacheRequestService::INCLUDES_FOLDER . '?' . http_build_query($_GET);
+} elseif ($action === BlitzVariable::DYNAMIC_INCLUDE_ACTION) {
+    $uri = http_build_query($_GET);
+}
+
+$cacheFolderPath = defined('BLITZ_CACHE_FOLDER_PATH') ? BLITZ_CACHE_FOLDER_PATH : CRAFT_BASE_PATH . '/web/cache/blitz';
+$path = $cacheFolderPath . '/' . $host . $uri . '/index.html';
+$path = str_replace(['//', '..'], ['/', ''], $path);
+
+if (!is_file($path)) {
+    return;
+}
+
+echo file_get_contents($path);
+exit();
