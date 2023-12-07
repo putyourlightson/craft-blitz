ea24d3a799ab5c19efa7d5bb8ba63490ffdb66a3
bencroker <ben@putyourlightson.com>
2023-12-07 16:52:16 -0600

    Cleanup

--- src/utilities/DiagnosticsUtility.php
+++ src/utilities/DiagnosticsUtility.php
@@ -9,10 +9,7 @@ use Craft;
 use craft\base\Element;
 use craft\base\Utility;
 use craft\db\Table;
-use craft\elements\db\ElementQueryInterface;
-use craft\helpers\Json;
 use putyourlightson\blitz\assets\BlitzAsset;
-use putyourlightson\blitz\helpers\RefreshCacheHelper;
 use putyourlightson\blitz\records\CacheRecord;
 use putyourlightson\blitz\records\ElementCacheRecord;
 use putyourlightson\blitz\records\ElementQueryCacheRecord;
@@ -141,51 +138,5 @@ class DiagnosticsUtility extends Utility
             ->orderBy(['count' => SORT_DESC])
             ->asArray()
             ->all();
-    }
-
-    public static function getElementsQuery(int $id, string $elementType): ElementQueryInterface
-    {
-        $elementIds = ElementCacheRecord::find()
-            ->select(['id'])
-            ->innerJoin(Table::ELEMENTS, 'id = elementId')
-            ->where([
-                'cacheId' => $id,
-                'type' => $elementType,
-            ])
-            ->asArray()
-            ->column();
-
-        /** @var Element $elementType */
-        return $elementType::find()
-            ->id($elementIds)
-            ->status(null);
-    }
-
-    public static function getElementQueriesQuery(int $id, string $elementQueryType): Query
-    {
-        return ElementQueryCacheRecord::find()
-            ->select(['params'])
-            ->innerJoinWith('elementQuery')
-            ->where([
-                'cacheId' => $id,
-                'type' => $elementQueryType,
-            ]);
-    }
-
-    public static function getElementQuerySql(string $elementQueryType, string $params): string
-    {
-        $params = Json::decodeIfJson($params);
-
-        // If json decode failed
-        if (!is_array($params)) {
-            return 'Invalid params.';
-        }
-
-        $elementQuery = RefreshCacheHelper::getElementQueryWithParams($elementQueryType, $params);
-
-        return $elementQuery
-            ->select(['elementId' => 'elements.id'])
-            ->createCommand()
-            ->getRawSql();
     }
 }
