a897fe47e5306a5c2fc12e21d4e59b0c97416348
bencroker <ben@putyourlightson.com>
2024-02-08 12:39:20 -0600

    Add code analysis

--- ecs.php
+++ ecs.php
@@ -4,12 +4,11 @@ use craft\ecs\SetList;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function(ECSConfig $ecsConfig): void {
-    $ecsConfig->parallel();
     $ecsConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests/pest',
         __FILE__,
     ]);
-
+    $ecsConfig->parallel();
     $ecsConfig->sets([SetList::CRAFT_CMS_4]);
 };
