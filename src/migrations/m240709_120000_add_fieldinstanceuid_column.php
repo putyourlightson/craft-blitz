191a5117d98cb091bb5ff26255621eb6f2c5ef84
bencroker <ben@putyourlightson.com>
2024-08-05 13:57:48 +0200

    Specify column order

--- src/migrations/m240709_120000_add_fieldinstanceuid_column.php
+++ src/migrations/m240709_120000_add_fieldinstanceuid_column.php
@@ -16,13 +16,13 @@ class m240709_120000_add_fieldinstanceuid_column extends Migration
         if ($this->db->columnExists(ElementFieldCacheRecord::tableName(), 'fieldId')) {
             ElementFieldCacheRecord::deleteAll();
             $this->dropColumn(ElementFieldCacheRecord::tableName(), 'fieldId');
-            $this->addColumn(ElementFieldCacheRecord::tableName(), 'fieldInstanceUid', $this->uid());
+            $this->addColumn(ElementFieldCacheRecord::tableName(), 'fieldInstanceUid', $this->uid()->after('elementId'));
         }
 
         if ($this->db->columnExists(ElementQueryFieldRecord::tableName(), 'fieldId')) {
             ElementQueryFieldRecord::deleteAll();
             $this->dropColumn(ElementQueryFieldRecord::tableName(), 'fieldId');
-            $this->addColumn(ElementQueryFieldRecord::tableName(), 'fieldInstanceUid', $this->uid());
+            $this->addColumn(ElementQueryFieldRecord::tableName(), 'fieldInstanceUid', $this->uid()->after('queryId'));
         }
 
         return true;
