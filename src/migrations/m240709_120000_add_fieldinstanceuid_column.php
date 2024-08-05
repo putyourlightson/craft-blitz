39c1af2cd2b9ca3fea98efdbb5a90ab3a71e7a59
bencroker <ben@putyourlightson.com>
2024-08-05 14:08:11 +0200

    Revert dropping foreign eye

--- src/migrations/m240709_120000_add_fieldinstanceuid_column.php
+++ src/migrations/m240709_120000_add_fieldinstanceuid_column.php
@@ -15,12 +15,14 @@ class m240709_120000_add_fieldinstanceuid_column extends Migration
     {
         if ($this->db->columnExists(ElementFieldCacheRecord::tableName(), 'fieldId')) {
             ElementFieldCacheRecord::deleteAll();
+            $this->dropForeignKeyIfExists(ElementFieldCacheRecord::tableName(), ['fieldId']);
             $this->dropColumn(ElementFieldCacheRecord::tableName(), 'fieldId');
             $this->addColumn(ElementFieldCacheRecord::tableName(), 'fieldInstanceUid', $this->uid()->after('elementId'));
         }
 
         if ($this->db->columnExists(ElementQueryFieldRecord::tableName(), 'fieldId')) {
             ElementQueryFieldRecord::deleteAll();
+            $this->dropForeignKeyIfExists(ElementQueryFieldRecord::tableName(), ['fieldId']);
             $this->dropColumn(ElementQueryFieldRecord::tableName(), 'fieldId');
             $this->addColumn(ElementQueryFieldRecord::tableName(), 'fieldInstanceUid', $this->uid()->after('queryId'));
         }
