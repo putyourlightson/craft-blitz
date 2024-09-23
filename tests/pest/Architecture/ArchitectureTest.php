a1132886ce6b6b0cbb7e76cb14a14221d41b57a4
bencroker <ben@putyourlightson.com>
2024-09-23 21:38:07 +0200

    Add test for `Craft::dd`

--- tests/pest/Architecture/ArchitectureTest.php
+++ tests/pest/Architecture/ArchitectureTest.php
@@ -4,6 +4,10 @@
  * Tests the architecture of the plugin.
  */
 
+test('Source code does not contain any `Craft::dd` statements')
+    ->expect(Craft::class)
+    ->not->toUse(['dd']);
+
 test('Source code does not contain any `var_dump` or `die` statements')
     ->expect(['var_dump', 'die'])
     ->not->toBeUsed();
