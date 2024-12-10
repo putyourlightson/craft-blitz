fad3d0bd6a6a0d54bad0445d654c796418adb913
bencroker <ben@putyourlightson.com>
2024-12-10 08:53:30 -0600

    Log stack trace

--- src/drivers/generators/local-generator-script.php
+++ src/drivers/generators/local-generator-script.php
@@ -104,7 +104,7 @@ return function(Channel $channel): void {
     try {
         $success = $app->run() == 0;
     } catch (Throwable $exception) {
-        $error = 'Page not cached because an error was encountered: ' . $exception->getMessage();
+        $error = 'Page not cached because an error was encountered: ' . $exception->getMessage() . ' ' . $exception->getTraceAsString();
         Blitz::$plugin->debug($error, [], $url);
     }
 
